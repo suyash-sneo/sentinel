0b562392b7db1eaf56854ac17de54a2c85097cb7
Suyash Raj <suyash10581108@gmail.com>
2025-04-26 12:11:41 -0500

    Added async TCP server to parse config

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ use std::process::{Command, Stdio, Child};
 use std::io::{BufReader, BufRead};
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::os::unix::process::CommandExt;
 use signal_hook::{consts::signal::*, iterator::Signals};
 use nix::sys::signal::{kill, Signal};
 use nix::unistd::Pid;
+
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 mod models;
 
@@ -34,7 +36,7 @@ fn listen_change() -> notify::Result<()> {
     }
 }
 
-fn parse_config(file_path: &str) -> models::config::Config {
+fn parse_config(file_path: String) -> models::config::Config {
 
     let config_file = File::open(file_path).expect("Error opening config file");
     let reader = BufReader::new(config_file);
@@ -86,11 +88,69 @@ fn launch_command(config: models::config::Config) {
     }
 }
 
-fn main() {
+fn handle_serve(arg1: &str) -> String {
 
-    let config = parse_config("/Users/sneo/dev/code/backend/sentinel/sample-configs/go-server.yaml");
+    let config_path = format!("/Users/sneo/dev/code/backend/sentinel/sample-configs/{}", arg1);
+    let config = parse_config(config_path);
 
     println!("Parsed config: {:#?}", config);
+    String::from("start handled")
+}
 
-    launch_command(config);
+fn handle_cmd(cmd : &str, arg1: &str) -> String {
+    match cmd {
+        "serve" => handle_serve(arg1),
+        _ => String::from("invalid command"),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let listener = TcpListener::bind("127.0.0.1:9109").await?;
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 4096];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                let data = String::from_utf8_lossy(&buf[0..n]);
+                println!("DATA READ: {:?}", data);
+
+                let input_parts: Vec<&str> = data.split_whitespace().collect();
+                if input_parts.len()<1 {
+                    eprintln!("Too few arguments");
+                    return;
+                }
+
+                let cmd = input_parts[0];
+                let mut arg1 = "";
+                if input_parts.len()>1 {
+                    arg1 = input_parts[1];
+                }
+
+                println!("Cmd: {0}, Arg: {1}", cmd, arg1);
+                let resp = handle_cmd(cmd, arg1) + "\n";
+
+                if let Err(e) = socket.write_all(resp.as_bytes()).await {
+                    eprintln!("Error writing response: {0}; err = {1:?}", resp, e);
+                    return;
+                }
+            }
+        });
+    }
+
+
+    // launch_command(config);
 }
